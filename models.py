e1081d792250d57fdf011884ddf401bc169f094c
Pihunovo <104624629+Pihunovo@users.noreply.github.com>
2025-01-15 16:45:07 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,14 +6,12 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'Users'
     id = Column(Integer, primary_key=True)
-    created_at = Column(Time, primary_key=False)
     username = Column(String, primary_key=False)
     password = Column(String, primary_key=False)
 
 class Interests(Base):
     __tablename__ = 'Interests'
     id = Column(Integer, primary_key=True)
-    created_at = Column(Time, primary_key=False)
     Activities = Column(String, primary_key=False)
     Groups = Column(String, primary_key=False)
     Languages = Column(String, primary_key=False)
